af08c38ab2027b5f663625ba69cf897badabf716
Mikalai Kazlou <mikalai_kazlou@epam.com>
2023-06-10 09:02:18 +0000

    refactor: code structuring

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,4 +1,7 @@
-const cds = require('@sap/cds')
+const cds = require('@sap/cds');
+const hdbext = require('@sap/hdbext');
+const dbClass = require('sap-hdbext-promisfied');
+
 module.exports = cds.service.impl(function () {
   const { Products } = this.entities()
 
@@ -17,12 +20,11 @@ module.exports = cds.service.impl(function () {
   this.on('GetSupplierInfo', async () => {
     try {
       const db = await cds.connect.to('db');
-      const dbClass = require('sap-hdbext-promisfied');
-      console.log(db.options);
-      let dbConn = new dbClass(await dbClass.createConnection(db.options.credentials));
-      const hdbext = require('@sap/hdbext');
-      const sp = await dbConn.loadProcedurePromisified(hdbext, null, 'get_supplier_info');
-      const output = await dbConn.callProcedurePromisified(sp, []);
+
+      const dbConnection = new dbClass(await dbClass.createConnection(db.options.credentials));
+      const sp = await dbConnection.loadProcedurePromisified(hdbext, null, 'get_supplier_info');
+      const output = await dbConnection.callProcedurePromisified(sp, []);
+
       console.log(output.results);
       return output.results;
     } catch (error) {
